7376977682dec205a20d18aa930d4d92ab8b6237
DanielAlba96 <danielalba1996@hotmail.es>
2017-03-31 20:26:07 +0200

    Añadido comentario en ParticipantServiceImpl

--- src/main/java/asw/dbUpdate/impl/ParticipantServiceImpl.java
+++ src/main/java/asw/dbUpdate/impl/ParticipantServiceImpl.java
@@ -10,23 +10,20 @@ import asw.dbUpdate.model.Suggestion;
 import asw.dbUpdate.model.VoteSuggestion;
 import asw.dbUpdate.repository.ParticipantRepository;
 import asw.dbUpdate.repository.SuggestionRepository;
-import asw.dbUpdate.repository.VoteCommentRepository;
 import asw.dbUpdate.repository.VoteSuggestionRepository;
+
+//CADA VEZ QUE SE CREE UN OBJETO NUEVO SE DEBE HACER DENTRO DE UN METODO TRANSACTIONAL DENTRO DE UN SERVICE PARA PODER USAR LA CLASE ASSOCIATIONS
 
 @Service
 public class ParticipantServiceImpl implements ParticipantService {
 
 	private ParticipantRepository pr;
-	private VoteCommentRepository vcr; // Lo añado ya aqui para el que haga lo
-										// comentarios y tenerlo todo unificado
 	private VoteSuggestionRepository vsr;
 	private SuggestionRepository sr;
 
 	@Autowired
-	public ParticipantServiceImpl(ParticipantRepository pr, VoteCommentRepository vcr, VoteSuggestionRepository vsr,
-			SuggestionRepository sr) {
+	public ParticipantServiceImpl(ParticipantRepository pr, VoteSuggestionRepository vsr, SuggestionRepository sr) {
 		this.pr = pr;
-		this.vcr = vcr;
 		this.vsr = vsr;
 		this.sr = sr;
 	}
