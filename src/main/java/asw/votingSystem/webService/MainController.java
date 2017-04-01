2af89d8e40ad1fe07a4e7013137d7177a776ac13
DanielAlba96 <danielalba1996@hotmail.es>
2017-04-01 13:43:01 +0200

    Manejo de errores
    Se redireccion a una pagina generica de error sea cual sea el fallo, de
    esta manera no se da informacion innecesaria al usuario sobre que error
    ha tenido lugar

--- src/main/java/asw/votingSystem/webService/MainController.java
+++ src/main/java/asw/votingSystem/webService/MainController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -42,5 +43,10 @@ public class MainController {
 			model.addAttribute("sugerencias", sugerencias);
 			return "index";
 		}
+	}
+
+	@ExceptionHandler(Exception.class)
+	public String errorPage() {
+		return "error";
 	}
 }
